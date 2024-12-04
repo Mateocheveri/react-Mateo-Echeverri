bd0f0bacb3ee28c12d0934381ec3de9c41fa5c7c
Mateo <cros746@gmail.com>
2024-12-03 21:33:44 -0500

    trabajo nuevo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Section from './components/Section';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <main>
+        <Section id='home' title='Home'>
+          <p>Bienvenido a nuestra pagina de inicio.</p>
+        </Section>
+        <Section id='about' title='About'>
+          <p>Infotmacion sobre nosotros.</p>
+        </Section>
+        <Section id='services' title='Services'>
+          <p>Detalles de nuestros servicios.</p>
+        </Section>
+        <Section id='contact' title='Contact'>
+          <p>Informacion de contacto.</p>
+        </Section>
+      </main>
+      <Footer />
     </div>
   );
 }
